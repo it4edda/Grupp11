7ec2714712c2210ef4bcfcb6f07e896be7580af9
Edmalm <nilswilliam2005@gmail.com>
2023-09-07 14:58:54 +0200

    nils incest

---
+++ DigGrupp11/Assets/Scripts/Other/Interaction.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] float          radius;
+    [SerializeField] protected bool canInteract;
+    [SerializeField] Animator       interactIcon;
+    Transform                       target;
+    protected virtual void Start()
+    {
+        //target = FindObjectOfType<PlayerMovement>().transform;
+    }
+    void Update()
+    {
+        InteractionPassive();
+
+        bool a = Vector2.Distance(transform.position, target.position) < radius;
+        interactIcon.SetBool("Showing", a);
+        
+        if (canInteract && a)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                InteractionActive();
+            }
+        }
+    }
+    protected virtual void InteractionPassive()
+    {
+        
+    }
+    protected virtual void InteractionActive()
+    {
+        canInteract = false;
+    }
+    void OnDrawGizmosSelected()
+    {
+     Gizmos.color = Color.red;
+     Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
+
