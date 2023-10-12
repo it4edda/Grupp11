44cf8cebaf117a158bc10eb4ec92ca03e9849af9
kairo6195 <143092129+kairo6195@users.noreply.github.com>
2023-10-12 16:08:59 +0200

    rtyu

---
+++ DigGrupp11/Assets/Scripts/Benjamin script/PlayerMovement.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float moveSpeed;
+
+    [SerializeField] float groundDrag;
+
+    [SerializeField] float jumpForce;
+    [SerializeField] float jumpCooldown;
+    [SerializeField] float airMultiplier;
+    bool readyToJump;
+
+    [Header("Keybimd")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    [SerializeField] float playerHeight;
+    [SerializeField] LayerMask Ground;
+    bool grounded;
+
+    [SerializeField] Transform orientation;
+
+    float horizantalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Ground);
+
+        MyInput();
+        SpeedControl();
+
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    private void MyInput()
+    {
+        horizantalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump))
+        }
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizantalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+}
---
+++ DigGrupp11/Assets/Scripts/Benjamin script/PlayerScrap.cs
@@ -0,0 +1,61 @@
+
+using UnityEngine;
+
+public class Playerscrap : MonoBehaviour
+{
+    [SerializeField] float mouseSensetivity = 3f;
+    [SerializeField] float movementSpeed = 3f;
+    [SerializeField] float mass = 3f;
+    [SerializeField] Transform cameraTransform;
+
+    CharacterController controller;
+    Vector3 velocity;
+    Vector2 look;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        UpdateGravity();
+        UpdateMovement();
+        UpdateLook();
+    }
+    void UpdateGravity()
+    {
+        var gravity = Physics.gravity * mass * Time.deltaTime;
+        velocity.y = controller.isGrounded ? -1f : velocity.y + gravity.y;
+    }
+
+    void UpdateMovement()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        var input = new Vector3();
+        input += transform.forward * y;
+        input += transform.forward * x;
+        input = Vector3.ClampMagnitude(input, 1f);
+
+        controller.Move((input * movementSpeed + velocity) * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void UpdateLook()
+    {
+        look.x += Input.GetAxis("Mouse X");
+        look.y += Input.GetAxis("Mouse Y");
+
+        look.y = Mathf.Clamp(look.y, -85f, 85f);
+
+        cameraTransform.localRotation = Quaternion.Euler(-look.y, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, look.x, 0);
+    }
+}
