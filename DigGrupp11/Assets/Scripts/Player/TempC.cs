14481fec1bdec753d6096ca2a08da29b335da320
Edmalm <nilswilliam2005@gmail.com>
2023-10-12 16:07:33 +0200

    asdasd

---
+++ DigGrupp11/Assets/Scripts/Player/TempC.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class TempC : MonoBehaviour
+{
+#region
+    [SerializeField] float mouseSensitivity = 1500f;
+
+    float xRotation = 0f;
+
+    Transform playerBody;
+#endregion
+    private void Awake()
+    {
+        playerBody = transform.parent.transform;
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        look();
+    }
+
+    void look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        //GetAxisRaw is instant, otherwise it lerps
+        //left right
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        //up down
+        xRotation               -= mouseY;
+        xRotation               =  Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation =  Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
--- DigGrupp11/Assets/Scripts/Player/TempP.cs
+++ DigGrupp11/Assets/Scripts/Player/TempP.cs
@@ -1,19 +1,105 @@
+using System;
 using UnityEngine;
- 
-public class TempP : MonoBehaviour {
- 
-#region "Variables"
-    public Rigidbody Rigid;
-    public float     MouseSensitivity;
-    public float     MoveSpeed;
-    public float     JumpForce;
-#endregion
-   
-    void Update ()
+
+public class TempP : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float walkSpeed = 5f;
+    [SerializeField] float runSpeed = 8f;
+    [SerializeField] float crouchSpeed = 2f;
+    [SerializeField] float crouchSize = 0.5f;
+    [SerializeField] float gravity = -29.46f; //-9.82
+    [SerializeField] float jumpHeight = 1.5f;
+    //[SerializeField] float dashPower = 3f;
+
+    [Header("Ground Check")]
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+
+    float xInput;
+    float zInput;
+    Vector3 movement;
+    Vector3 velocity;
+    bool isGrounded;
+    //bool canDash;
+    bool isCrouching;
+
+    CharacterController characterController;
+
+    private void Awake()
     {
-        Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0)));
-        Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
-        if (Input.GetKeyDown("space"))
-            Rigid.AddForce(transform.up * JumpForce);
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ground();
+        Crouch();
+        Move();
+        Jump();
+        //Dash();
+        Gravity();
+    }
+    void Ground()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
+    void Crouch()
+    {
+        //not too proud of these but 
+        // if it works it works
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = true;
+            transform.localScale = new Vector3(1, crouchSize, 1);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            isCrouching= false;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    void Move()
+    {
+        float movementSpeed = 1;
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+
+        movement = Vector3.ClampMagnitude((transform.right * xInput) + (transform.forward * zInput), 1f);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = runSpeed;
+        }
+        else if (isCrouching)
+        {
+            movementSpeed = crouchSpeed;
+        }
+        else
+        {
+            movementSpeed = walkSpeed;
+        }
+
+        characterController.Move(movement * (movementSpeed * Time.deltaTime));
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = MathF.Sqrt(jumpHeight * -2f * gravity);   
+        }
+    }
+
+    void Gravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
