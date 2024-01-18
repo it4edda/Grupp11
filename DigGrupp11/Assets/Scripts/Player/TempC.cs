517f53e08449f7f2f55c3416b1f309aecec1c407
Edmalm <nilswilliam2005@gmail.com>
2024-01-18 14:18:51 +0100

    temp poset

--- DigGrupp11/Assets/Scripts/Player/TempC.cs
+++ DigGrupp11/Assets/Scripts/Player/TempC.cs
@@ -1,14 +1,17 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class TempC : MonoBehaviour
 {
-    //kys leo
     //bool Leoiscool = true;
 #region
+
     [SerializeField] float mouseSensitivity = 1500f;
+    [SerializeField] float mouseSensitivityY = 1500f;
 
-    float xRotation = 0f;
-
+    float     xRotation = 0f;
+    float     yRotation = 0f;
     Rigidbody rb;
     Transform playerBody;
 #endregion
@@ -16,6 +19,7 @@ public class TempC : MonoBehaviour
     {
         playerBody = transform.parent.transform;
         rb = GetComponentInParent<Rigidbody>();
+        Debug.Log(rb);
     }
 
     void Start()
@@ -25,30 +29,29 @@ public class TempC : MonoBehaviour
 
     void Update()
     {
-        look();
-    }
-
-    void look()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        //GetAxisRaw is instant, otherwise it lerps
-        //left right
-        
-        
-        
-        //works 
-        playerBody.Rotate(Vector3.up * mouseX);
-
-        
-        
-        //Vector3 a = new Vector3(mouseX,0, 0);
-        //rb.AddTorque(a * 100, ForceMode.Force);
-        Debug.Log("WILLIAM JOBBAR HÄR");
-
-        //up down
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityY * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY * Time.deltaTime;
         xRotation               -= mouseY;
+        yRotation               += mouseX;
         xRotation               =  Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation =  Quaternion.Euler(xRotation, 0f, 0f);
+transform.parent.transform.localRotation = Quaternion.Euler(0,yRotation,0);
+    }
+    void FixedUpdate()
+    {
+        //Look();
+    }
+
+    void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+
+        Vector3 torque = Vector3.up * mouseX;
+
+        Debug.Log("Mouse X: "                  + mouseX);
+        Debug.Log("Torque: "                   + torque);
+        Debug.Log("Current Angular Velocity: " + rb.angularVelocity);
+
+        rb.AddTorque(torque, ForceMode.VelocityChange);
     }
 }
