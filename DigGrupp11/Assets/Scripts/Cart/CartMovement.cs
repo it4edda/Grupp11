3d9f89fe7134d96def6e7a02ae508e7e11edcf96
Monen10 <tovasimoneberntsson@gmail.com>
2024-01-30 11:43:55 +0100

    ghkl

--- DigGrupp11/Assets/Scripts/Cart/CartMovement.cs
+++ DigGrupp11/Assets/Scripts/Cart/CartMovement.cs
@@ -13,12 +13,14 @@ public class CartMovement : MonoBehaviour
     [SerializeField] bool havePlayer = false;
 
     [SerializeField] Rigidbody rb;
-    TempP     player;
+    TempP player;
+    RotationThingy RotationThingy;
 
     private void Start()
     {
         player = FindObjectOfType<TempP>();
-        rb     = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        RotationThingy = FindObjectOfType<RotationThingy>();
     }
 
     private void Update()
@@ -28,11 +30,11 @@ public class CartMovement : MonoBehaviour
             return;
         }
         
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && RotationThingy.CanRotateRight)
         {
             transform.Rotate(new Vector3(0, -rotateSpeed * Time.deltaTime, 0)); 
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && RotationThingy.CanRotateLeft)
         {
             transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
         }
@@ -49,13 +51,13 @@ public class CartMovement : MonoBehaviour
 
 
     public void SetHavePlayer(bool a)
-    { 
-        havePlayer       = a;
+    {
+        havePlayer = a;
         Debug.Log("did cart stuff" + " ---  holding cart =" + a);
         TempP b = FindObjectOfType<TempP>();
         TempC c = FindObjectOfType<TempC>();
         c.isHoldingCart = a;
-        b.CanMove            = !a;
+        b.CanMove = !a;
         //b.transform.parent   = a ? transform : null;
     }
 }
