ef3052645454effadff15bc613d93b8b08e9f6dd
Edmalm <nilswilliam2005@gmail.com>
2024-04-25 18:12:17 +0200

    ADDA

--- DigGrupp11/Assets/Scripts/Checkout.cs
+++ DigGrupp11/Assets/Scripts/Checkout.cs
@@ -54,9 +54,9 @@ public class Checkout : MonoBehaviour
     }
     IEnumerator DestroyMoney(GameObject money)
     {
-        money.GetComponent<Money>().canBePickedUp = false;
+        money.GetComponentInParent<Money>().canBePickedUp = false;
         audioSource.PlayOneShot(kaCaching);
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.1f);
         Destroy(money);
     }
 
